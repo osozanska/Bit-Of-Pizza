7880de780c2ea90119617a07e148acbd367accf0
podziemska <podziemskablanka@gmail.com>
2025-04-22 22:36:53 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,62 @@
 #include "raylib.h"
 
+enum Ekran { MENU, GRA, WYJSCIE }; // typ dla wartosci stałych , żeby nie pisac np 0,1,2 itp
+
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    InitWindow(screenWidth, screenHeight, "Raylib C++ Menu");
+    const int szerokoscOkna = 800, wysokoscOkna = 600;
+    InitWindow(szerokoscOkna, wysokoscOkna, "BIT OF PIZZA");
     SetTargetFPS(60);
+    Texture2D tloMenu = LoadTexture("obrazki/menu.png");
 
-    bool gameStarted = false;
-
+    Ekran aktualnyEkran = MENU;
     while (!WindowShouldClose()) {
-        if (!gameStarted && IsKeyPressed(KEY_ENTER)) gameStarted = true;
-        if (!gameStarted && IsKeyPressed(KEY_ESCAPE)) break;
-
         BeginDrawing();
-        ClearBackground(BLACK);
 
-        if (!gameStarted) {
-            DrawText("MENU GLOWNE", screenWidth/2 - 120, 150, 40, WHITE);
-            DrawText("Nacisnij [ENTER], aby rozpoczac", screenWidth/2 - 200, 250, 20, WHITE);
-            DrawText("Nacisnij [ESC], aby wyjsc", screenWidth/2 - 150, 300, 20, WHITE);
-        } else {
-            DrawText("GRA TRWA!", 320, 280, 30, WHITE);
+        if (aktualnyEkran == MENU) {
+            DrawTexture(tloMenu, 0, 0, WHITE);
+
+            // MeasureText: Oblicza szerokość tekstu, aby można go było wyśrodkować
+            int tekstX = szerokoscOkna / 2 - MeasureText("BIT OF PIZZA", 40) / 2;
+            int tekstY = 100;
+
+            DrawText("BIT OF PIZZA", tekstX - 2, tekstY, 40, BLACK);
+            DrawText("BIT OF PIZZA", tekstX + 2, tekstY, 40, BLACK); 
+            DrawText("BIT OF PIZZA", tekstX, tekstY - 2, 40, BLACK); 
+            DrawText("BIT OF PIZZA", tekstX, tekstY + 2, 40, BLACK); 
+            DrawText("BIT OF PIZZA", tekstX - 1, tekstY - 1, 40, BLACK); 
+            DrawText("BIT OF PIZZA", tekstX + 1, tekstY - 1, 40, BLACK); 
+            DrawText("BIT OF PIZZA", tekstX - 1, tekstY + 1, 40, BLACK); 
+            DrawText("BIT OF PIZZA", tekstX + 1, tekstY + 1, 40, BLACK); 
+            DrawText("BIT OF PIZZA", tekstX, tekstY, 40, WHITE);            
+
+            Rectangle przyciskStart = { szerokoscOkna/2-100, 200, 200, 50 }; // x: szerokoscOkna/2-100 y: 200 szerokość: 200 wysokość: 50
+            DrawRectangleRec(przyciskStart, ORANGE);
+            DrawText("Start", przyciskStart.x + 70, przyciskStart.y + 10, 30, WHITE);
+
+            Rectangle przyciskWyjscie = { szerokoscOkna /2-100, 340, 200, 50 };
+            DrawRectangleRec(przyciskWyjscie, ORANGE);
+            DrawText("Wyjscie", przyciskWyjscie.x + 60, przyciskWyjscie.y + 10, 30, WHITE);
+
+            if (CheckCollisionPointRec(GetMousePosition(), przyciskStart) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                aktualnyEkran = GRA;
+            if (CheckCollisionPointRec(GetMousePosition(), przyciskWyjscie) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                aktualnyEkran = WYJSCIE;
+        }
+        else {
+            ClearBackground(RAYWHITE);
+
+            if (aktualnyEkran == GRA) {
+                DrawText("ESC", 200, 250, 30, DARKGREEN);
+                if (IsKeyPressed(KEY_ESCAPE)) aktualnyEkran = MENU;
+            } else if (aktualnyEkran == WYJSCIE) {
+                CloseWindow();
+                break;
+            }
         }
 
         EndDrawing();
     }
 
-    CloseWindow();
+    UnloadTexture(tloMenu); 
     return 0;
 }
