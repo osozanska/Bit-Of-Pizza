abeff980c0e01b133f51cb86bab3df4cf675bdf5
podziemska <podziemskablanka@gmail.com>
2025-05-06 17:20:59 +0200

    klient.cpp

--- klient.cpp
+++ klient.cpp
@@ -3,23 +3,33 @@
 
 Klienci::Klienci() {
     x = 700;
-    y = 0;
+    y = 100;
     predkoscY = 2;
     czyAktywny = true;
     zamowieniePrzyjete = false;
     czasStart = GetTime();  
     czasNaZamowienie = 120; 
+    klient = LoadTexture("obrazki/klient.png");
+}
+
+Klienci::Klienci(int x, int y, int predkoscY, int czasNaZamowienie) {
+    this->x = x;
+    this->y = y;
+    this->predkoscY = predkoscY;
+    this->czyAktywny = true;
+    this->zamowieniePrzyjete = false;
+    this->czasStart = GetTime();
+    this->czasNaZamowienie = czasNaZamowienie; 
+    klient = LoadTexture("obrazki/klient.png");
 }
 
 void Klienci::aktualizuj() {
-    if (czyAktywny == false) return; // zmiana z !czyAktywny, żeby było bardziej czytelniej jak w każdym.
+    if (czyAktywny == false) return;
 
-    if (zamowieniePrzyjete == true) {
-        if (y < 300) {
-            y += predkoscY;
-        } else {
-            y = 300; // zatrzymanie
-        }
+    if (y < 225) {
+        y += predkoscY;
+    } else {
+        y = 225;
     }
 
     if (pozostalyCzas() <= 0) {
@@ -30,22 +40,22 @@ void Klienci::aktualizuj() {
 void Klienci::rysuj() {
     if (czyAktywny == false) return;
 
-    DrawRectangle(x, y, 30, 40, BLUE);
+    DrawTexture(klient, x, y, WHITE);
     DrawText(TextFormat("Pozostaly czas: %i s", pozostalyCzas()), 10, 10, 20, WHITE);
 
     if (!zamowieniePrzyjete) {
         DrawText("Podejdz i kliknij myszka, by przyjac zamowienie", 10, 40, 20, WHITE);
     } else {
-        DrawText("Zamwienie przyjete!", 10, 40, 20, GREEN);
+        DrawText("Zamowienie przyjete!", 10, 40, 20, GREEN);
     }
 }
 
 void Klienci::sprawdzInterakcje(int graczX, int graczY) {
-    if (czyAktywny == false || zamowieniePrzyjete) return; // zamowienie przyjete jest w domysle false
+    if (czyAktywny == false || zamowieniePrzyjete || y < 225) return;
 
-    Rectangle klient = { (float)x, (float)y, 30, 40 };
+    Rectangle klientrec = { (float)x, (float)y, (float)klient.width, (float)klient.height }; 
 
-    if (CheckCollisionPointRec(GetMousePosition(), klient) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+    if (CheckCollisionPointRec(GetMousePosition(), klientrec) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
         zamowieniePrzyjete = true;
         czasStart = GetTime(); 
     }
