f3476ea43ad58e4fd579bc4d4dfc693038fe6e0a
podziemska <podziemskablanka@gmail.com>
2025-05-07 14:31:59 +0200

    klient.hpp

--- klient.hpp
+++ klient.hpp
@@ -1,5 +1,8 @@
 #ifndef KLIENCI_HPP
 #define KLIENCI_HPP
+#include <iostream>
+#include <string>
+using namespace std;
 
 #include "raylib.h"
 
@@ -12,17 +15,29 @@ private:
     bool zamowieniePrzyjete;
     float czasStart; 
     int czasNaZamowienie;
+    int zamowionaPizzaID;
+    bool pizzaDodana;
+    bool sosDodany;
+    bool serDodany;
+    bool pieczarkiDodane; 
+    bool cebulaDodana;
+    bool peperoniDodane;
+    bool piecPizzaDodane;
+    bool czyOddana;
     Texture2D klient;
 
 public:
     Klienci(); 
-    Klienci(int x, int y, int predkoscY, int czasNaZamowienie); 
+    Klienci(int x, int y, int predkoscY, int czasNaZamowienie, int zamowionaPizzaID, bool pizzaDodana, bool sosDodany, bool peperoniDodane, bool pieczarkiDodane, bool cebulaDodana, bool czyOddana); 
 
     void aktualizuj();                     
     void rysuj();                          
     void sprawdzInterakcje(int graczX, int graczY); 
     int pozostalyCzas();                  
-    bool aktywny();              
+    bool aktywny();   
+    int getPizzaId();
+    void wyloswaneId();
+    string zamowionaPizza();     
 };
 
 #endif
