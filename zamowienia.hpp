4750b195af2b49da1f660e1cc042d61d8e8e06a4
podziemska <podziemskablanka@gmail.com>
2025-05-13 21:07:40 +0200

    Update zamowienia.hpp

--- zamowienia.hpp
+++ zamowienia.hpp
@@ -15,7 +15,7 @@ public:
     Zamowienie();
     Zamowienie(int zamowionaPizzaID, bool czyZadowolony, int punkty);
     void wylosujPizzaID();
-    void sprawdzPoprawnosc(bool pizzaDodana, bool sosDodany, bool serDodany, bool peperoniDodane, bool pieczarkiDodane, bool cebulaDodana,int statusPieczenia);
+    void sprawdzPoprawnosc(bool pizzaDodana, bool sosDodany, bool serDodany, bool peperoniDodane, bool pieczarkiDodane, bool cebulaDodana,bool popranwiePrzypieczona);
     string tekstZamowienia() const;
     bool getCzyZadowolony() const;
     int getPunkty() const;
